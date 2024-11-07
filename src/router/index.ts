2b1fe85d10ce45379988d11dce0b3d67fb353759
saiporala <sai@brandingarc.com>
2024-11-07 12:53:32 +0530

    update the route

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,9 +10,9 @@ import { useAuthStore } from '@/stores/userStore';
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
-    meta: { requiresAuth: true },
+    name: 'Login',
+    component: Login,
+    // meta: { requiresAuth: true },
   },
   {
     path: '/login',
