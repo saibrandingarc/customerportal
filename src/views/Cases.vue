67bb0b33e5945c482677f34b368668d245b702db
saiporala <saikrishnaporala@gmail.com>
2025-07-03 18:31:04 +0530

    case ui margin

--- src/views/Cases.vue
+++ src/views/Cases.vue
@@ -5,143 +5,141 @@
   <div v-if="loading" class="spinner-overlay">
     <div class="spinner"></div>
   </div>
-  <div class="m-4">
-    <v-card>
-      <v-row>
-        <v-col cols="12" sm="12">
-          <v-data-table :headers="headers" :items="openitems" item-key="id" :sort-by="[{ key: 'Case_Number', order: 'asc' }]">
-            <template v-slot:top>
-              <v-toolbar flat>
-                <v-toolbar-title>Open Cases</v-toolbar-title>
-                <v-divider class="mx-8" inset vertical></v-divider>
-                <v-spacer></v-spacer>
-                <v-dialog v-model="dialog" max-width="800px">
-                  <template v-slot:activator="{ props }">
-                    <v-btn
-                      class="text-none"
-                      color="#198fd9"
-                      size="large"
-                      variant="flat"
-                      v-bind="props"
-                    >
-                    New Case
-                    </v-btn>
-                  </template>
-                  <v-card>
-                    <v-card-title>
-                      <span class="text-h5">{{ formTitle }}</span>
-                    </v-card-title>
+  <v-card class="m-4">
+    <v-row>
+      <v-col cols="12" sm="12">
+        <v-data-table :headers="headers" :items="openitems" item-key="id" :sort-by="[{ key: 'Case_Number', order: 'asc' }]">
+          <template v-slot:top>
+            <v-toolbar flat>
+              <v-toolbar-title>Open Cases</v-toolbar-title>
+              <v-divider class="mx-8" inset vertical></v-divider>
+              <v-spacer></v-spacer>
+              <v-dialog v-model="dialog" max-width="800px">
+                <template v-slot:activator="{ props }">
+                  <v-btn
+                    class="text-none"
+                    color="#198fd9"
+                    size="large"
+                    variant="flat"
+                    v-bind="props"
+                  >
+                  New Case
+                  </v-btn>
+                </template>
+                <v-card>
+                  <v-card-title>
+                    <span class="text-h5">{{ formTitle }}</span>
+                  </v-card-title>
 
-                    <v-card-text>
-                        <v-row>
-                          <v-col cols="12" md="12" sm="12">
-                            <v-text-field v-model="editedItem.Subject" clearable label="Subject" variant="outlined"></v-text-field>
-                          </v-col>
-                          <v-col cols="12" md="12" sm="12">
-                            <v-textarea v-model="editedItem.Description" clearable label="Description" variant="outlined"></v-textarea>
-                          </v-col>
-                        </v-row>
-                    </v-card-text>
+                  <v-card-text>
+                      <v-row>
+                        <v-col cols="12" md="12" sm="12">
+                          <v-text-field v-model="editedItem.Subject" clearable label="Subject" variant="outlined"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" md="12" sm="12">
+                          <v-textarea v-model="editedItem.Description" clearable label="Description" variant="outlined"></v-textarea>
+                        </v-col>
+                      </v-row>
+                  </v-card-text>
 
-                    <v-card-actions class="mr-4">
-                      <v-btn class="text-none mb-4" color="blue-darken-1" variant="text" @click="close">Cancel</v-btn>
-                      <v-btn class="text-none mb-4" color="indigo-darken-3" size="large" variant="flat" @click="save">Save</v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </v-dialog>
-                <v-dialog v-model="dialogDelete" max-width="500px">
-                  <v-card>
-                    <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
-                    <v-card-actions>
-                      <v-spacer></v-spacer>
-                      <v-btn color="blue-darken-1" variant="text" @click="closeDelete">Cancel</v-btn>
-                      <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirm">OK</v-btn>
-                      <v-spacer></v-spacer>
-                    </v-card-actions>
-                  </v-card>
-                </v-dialog>
-              </v-toolbar>
-            </template>
-            <template #item.Case_Number="{ item }">
-              {{ item.Case_Number ? item.Case_Number.slice(-8) : 'N/A' }}
-            </template>
-            <template v-slot:item.actions="{ item }">
-              <v-btn-group variant="outlined" divided>
-                <v-btn icon="mdi-eye" @click="viewItem(item)"></v-btn>
-                <v-btn icon="mdi-pencil" @click="editItem(item)"></v-btn>
-                <v-btn icon="mdi-delete" @click="deleteItem(item)"></v-btn>
-              </v-btn-group>
-            </template>
-          </v-data-table>
-        </v-col>
-      </v-row>
-    </v-card>
-    <v-spacer></v-spacer>
-    <v-card class="mt-4">
-      <v-row>
-        <v-col cols="12" sm="12">
-          <v-data-table :headers="headers" :items="closeditems" item-key="id" :sort-by="[{ key: 'Case_Number', order: 'asc' }]">
-            <template v-slot:top>
-              <v-toolbar flat>
-                <v-toolbar-title>Closed Cases</v-toolbar-title>
-                <v-divider class="mx-8" inset vertical></v-divider>
-                <v-spacer></v-spacer>
-                <v-dialog v-model="dialog" max-width="800px">
-                  <v-card>
-                    <v-card-title>
-                      <span class="text-h5">{{ formTitle }}</span>
-                    </v-card-title>
+                  <v-card-actions class="mr-4">
+                    <v-btn class="text-none mb-4" color="blue-darken-1" variant="text" @click="close">Cancel</v-btn>
+                    <v-btn class="text-none mb-4" color="indigo-darken-3" size="large" variant="flat" @click="save">Save</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+              <v-dialog v-model="dialogDelete" max-width="500px">
+                <v-card>
+                  <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue-darken-1" variant="text" @click="closeDelete">Cancel</v-btn>
+                    <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirm">OK</v-btn>
+                    <v-spacer></v-spacer>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-toolbar>
+          </template>
+          <template #item.Case_Number="{ item }">
+            {{ item.Case_Number ? item.Case_Number.slice(-8) : 'N/A' }}
+          </template>
+          <template v-slot:item.actions="{ item }">
+            <v-btn-group variant="outlined" divided>
+              <v-btn icon="mdi-eye" @click="viewItem(item)"></v-btn>
+              <v-btn icon="mdi-pencil" @click="editItem(item)"></v-btn>
+              <v-btn icon="mdi-delete" @click="deleteItem(item)"></v-btn>
+            </v-btn-group>
+          </template>
+        </v-data-table>
+      </v-col>
+    </v-row>
+  </v-card>
+  <v-spacer></v-spacer>
+  <v-card class="mt-4">
+    <v-row>
+      <v-col cols="12" sm="12">
+        <v-data-table :headers="headers" :items="closeditems" item-key="id" :sort-by="[{ key: 'Case_Number', order: 'asc' }]">
+          <template v-slot:top>
+            <v-toolbar flat>
+              <v-toolbar-title>Closed Cases</v-toolbar-title>
+              <v-divider class="mx-8" inset vertical></v-divider>
+              <v-spacer></v-spacer>
+              <v-dialog v-model="dialog" max-width="800px">
+                <v-card>
+                  <v-card-title>
+                    <span class="text-h5">{{ formTitle }}</span>
+                  </v-card-title>
 
-                    <v-card-text>
-                        <v-row>
-                          <v-col cols="12" md="12" sm="12">
-                            <v-text-field v-model="editedItem.Subject" clearable label="Subject" variant="outlined"></v-text-field>
-                          </v-col>
-                          <v-col cols="12" md="12" sm="12">
-                            <v-textarea v-model="editedItem.Description" clearable label="Description" variant="outlined"></v-textarea>
-                          </v-col>
-                        </v-row>
-                    </v-card-text>
+                  <v-card-text>
+                      <v-row>
+                        <v-col cols="12" md="12" sm="12">
+                          <v-text-field v-model="editedItem.Subject" clearable label="Subject" variant="outlined"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" md="12" sm="12">
+                          <v-textarea v-model="editedItem.Description" clearable label="Description" variant="outlined"></v-textarea>
+                        </v-col>
+                      </v-row>
+                  </v-card-text>
 
-                    <v-card-actions class="mr-4">
-                      <v-btn class="text-none mb-4" color="blue-darken-1" variant="text" @click="close">Cancel</v-btn>
-                      <v-btn class="text-none mb-4" color="indigo-darken-3" size="large" variant="flat" @click="save">Save</v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </v-dialog>
-                <v-dialog v-model="dialogDelete" max-width="500px">
-                  <v-card>
-                    <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
-                    <v-card-actions>
-                      <v-spacer></v-spacer>
-                      <v-btn color="blue-darken-1" variant="text" @click="closeDelete">Cancel</v-btn>
-                      <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirm">OK</v-btn>
-                      <v-spacer></v-spacer>
-                    </v-card-actions>
-                  </v-card>
-                </v-dialog>
-              </v-toolbar>
-            </template>
-            <template #item.Case_Number="{ item }">
-              {{ item.Case_Number ? item.Case_Number.slice(-8) : 'N/A' }}
-            </template>
-            <template v-slot:item.actions="{ item }">
-              <v-btn-group variant="outlined" divided>
-                <v-btn icon="mdi-eye" @click="viewItem(item)"></v-btn>
-                <!-- <v-btn icon="mdi-pencil" @click="editItem(item)"></v-btn>
-                <v-btn icon="mdi-delete" @click="deleteItem(item)"></v-btn> -->
-              </v-btn-group>
-              <!-- <v-icon class="me-2" color="red" size="large" @click="editItem(item)">mdi-pencil</v-icon>
-              <v-icon size="large" color="red" @click="deleteItem(item)">mdi-delete</v-icon> -->
-            </template>
-          </v-data-table>
-          <v-snackbar v-model="snackbar" :timeout="3000">
-            {{ text }}
-          </v-snackbar>
-        </v-col>
-      </v-row>
-    </v-card>
-  </div>
+                  <v-card-actions class="mr-4">
+                    <v-btn class="text-none mb-4" color="blue-darken-1" variant="text" @click="close">Cancel</v-btn>
+                    <v-btn class="text-none mb-4" color="indigo-darken-3" size="large" variant="flat" @click="save">Save</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+              <v-dialog v-model="dialogDelete" max-width="500px">
+                <v-card>
+                  <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue-darken-1" variant="text" @click="closeDelete">Cancel</v-btn>
+                    <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirm">OK</v-btn>
+                    <v-spacer></v-spacer>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-toolbar>
+          </template>
+          <template #item.Case_Number="{ item }">
+            {{ item.Case_Number ? item.Case_Number.slice(-8) : 'N/A' }}
+          </template>
+          <template v-slot:item.actions="{ item }">
+            <v-btn-group variant="outlined" divided>
+              <v-btn icon="mdi-eye" @click="viewItem(item)"></v-btn>
+              <!-- <v-btn icon="mdi-pencil" @click="editItem(item)"></v-btn>
+              <v-btn icon="mdi-delete" @click="deleteItem(item)"></v-btn> -->
+            </v-btn-group>
+            <!-- <v-icon class="me-2" color="red" size="large" @click="editItem(item)">mdi-pencil</v-icon>
+            <v-icon size="large" color="red" @click="deleteItem(item)">mdi-delete</v-icon> -->
+          </template>
+        </v-data-table>
+        <v-snackbar v-model="snackbar" :timeout="3000">
+          {{ text }}
+        </v-snackbar>
+      </v-col>
+    </v-row>
+  </v-card>
 </template>
 
 <script lang="ts" setup>
