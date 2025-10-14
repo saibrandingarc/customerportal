d527951d8c90b180bb39a12c33176d44af81e230
saiporala <saikrishnaporala@gmail.com>
2025-10-14 17:15:33 +0530

    update api url directly in config.ts

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,2 +1,2 @@
 // src/config.ts
-export const API_BASE_URL = import.meta.env.VITE_API_URL;
+export const API_BASE_URL = "https://customerportalapi-g9eneccnc0eghse3.eastus2-01.azurewebsites.net";//import.meta.env.VITE_API_URL;
