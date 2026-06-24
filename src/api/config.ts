9a5bb8abce460b7ffc16d1949f42ede5de41b7ad
varnarc <varnarc25@gmail.com>
2026-06-24 20:50:00 +0530

    commit env and config changes

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,11 +1,9 @@
 // src/config.ts
-<<<<<<< HEAD
-export const API_BASE_URL = "https://customerportalapitest-g4ghhxakewedafak.eastus2-01.azurewebsites.net";//import.meta.env.VITE_API_URL;
-=======
+// export const API_BASE_URL = "https://customerportalapitest-g4ghhxakewedafak.eastus2-01.azurewebsites.net";//import.meta.env.VITE_API_URL;
 export const API_BASE_URL =
   import.meta.env.VITE_API_URL ??
   'https://customerportalapitest-g4ghhxakewedafak.eastus2-01.azurewebsites.net';
 
 export const GOOGLE_DRIVE_FOLDER_URL =
   'https://drive.google.com/drive/folders/1TiHjrFhsimDHb5VphpuNkxaCyIenAeRh?usp=drive_link';
->>>>>>> main
+
